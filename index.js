e38c0d7016b1d1c0cd10484d7e2a41b36c812705
gustavovargas511 <gustavovargas511@gmail.com>
2023-12-31 19:02:42 -0600

    unnecessary comments removal

--- index.js
+++ index.js
@@ -12,11 +12,5 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use("/api/animals/v1", animalRouter);
 
-// app.get("/", (req, res) => {
-//     res.send('The beggining...');
-// })
-
-//mongo connection
-//mongoose.connect(process.env.MONGO_URI).then(()=>{console.log("Connected...")})
 
 app.listen(port, () => console.log('Server listening on port', port));
